bf0d468e229313f7d17b22a9e82b2727bae44c2a
eramoss <edulramos@outlook.com>
2024-03-21 08:49:13 -0300

    make it beauty

--- algebras/examples/ju_problem.py
+++ algebras/examples/ju_problem.py
@@ -6,11 +6,16 @@ import numpy as np
 
 rule = "2*i if i >= j, -j if j > i"
 matrix_4_4 = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-matrix = create_matrix_by_rule(matrix_4_4, rule)
-matrix = echelon_form(matrix)
+
 
 
 def print_matrix(matrix):
     print(np.array2string(matrix, separator=', ', formatter={'all': lambda x: "\t"+ str(x)}))
 
+matrix = create_matrix_by_rule(matrix_4_4, rule)
+print("Matrix by rule: ")
 print_matrix(np.array(matrix))
+print("\nEchelon form: ")
+matrix = echelon_form(matrix)
+print_matrix(np.array(matrix))
+print("Determinant: ", det_by_echelon_form(matrix))
